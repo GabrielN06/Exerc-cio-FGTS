feda1cdd94a8965305c4a9f585a0b04ef116e9a4
Gabriel <gabrielnunesdacostaanselmo@gmail.com>
2025-03-24 09:06:34 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double salario, fgts;
+Console.Write("Digite o salário para calcular o FGTS: (R$)..:");
+
+salario = Convert.ToDouble(Console.ReadLine());
+fgts = salario * 0.08;
+
+Console.WriteLine($"\nFGTS: R${fgts:F2} ");
+
+
+
